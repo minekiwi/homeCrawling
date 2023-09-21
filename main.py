0366748b11f638510537e31ee5aaf422b969913f
KiwiZip <45496769+minekiwi@users.noreply.github.com>
2023-09-21 13:40:36 +0000

    config 기능 추가

--- main.py
+++ main.py
@@ -1,31 +1,53 @@
 # < import modules >
-import configparser
+import time
 from datetime import datetime
+from datetime import date
 
 # < import class >
 import util.DataManager as DataManager
+import system.Crawling as Crawling
+from util.JsonFile import Json
+
+# < final datas >
+DATE_FORMAT = '%Y-%m-%d %Hh'
+
+config = Json("config", '')
 
 def initConfig():
     time = datetime.now()
 
-    config = configparser.ConfigParser()
-    config = {}
-    config['update'] = time.strftime('%Y-%m-%d %H:%M:%S')
+    config.resetData()
+    updateCycle = {"day":7}
+    config.addData("updateCycle", updateCycle)
+    
+    str_datetime = datetime.strftime(time, DATE_FORMAT)
+    config.addData("updateLast", str_datetime)
 
-    with open('config.ini', 'w', encoding='utf-8') as configfile:
-        config.write(configfile)
+    config.saveData()
 
 def readConfig(read):
-    config = configparser.ConfigParser()    
-    config.read('config.ini', encoding='utf-8') 
+    return config.loadData()[read]
 
-    return config[read]
+def checkUpdate():
+    currentTime = datetime.now()
+    updatedTime = readConfig("updateLast") 
+    updatedTime = time.mktime(datetime.strptime(updatedTime, DATE_FORMAT).timetuple())
+    updatedTime = datetime.fromtimestamp(updatedTime)
+    diffDay = (currentTime - updatedTime).days
+
+    if (diffDay >= config.loadData()['updateCycle']['day']):
+        print("renewal crawl data...")
 
 def main():
-    config = DataManager.read("config")
-    crawlTime = datetime.now()
-    if (config == False):
+    configVaild = DataManager.isVaild("config.json")
+
+    print("initalizing...")
+
+    if (configVaild == False):
         initConfig()
+
+    print("done...!")
+    
     
 if __name__ == '__main__':
     main()
